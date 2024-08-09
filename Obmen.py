4f4d57e3d2036938b33114387a5e1a01be8396d4
Admin <lea84@rambler.ru>
2024-08-09 19:58:10 +0300

    Создана функция exchange для получения из интернета курсов валют

--- Obmen.py
+++ Obmen.py
@@ -4,6 +4,22 @@ import requests
 import json
 
 
+def exchange():
+    code = entry.get().upper()
+    if code:
+        try:
+            response = requests.get(f'https://open.er-api.com/v6/latest/USD')
+            response.raise_for_status()
+            data = response.json()
+            if code in data['rates'][code]:
+                exchange_rate = data['rates'][code]
+                mb.showinfo("Курс обмена",f"Курс к доллару: {exchange_rate:.1f}{code} за 1 доллар")
+            else:
+                mb.showerror("Ошибка", f"Валюта {code} не найдена")
+        except Exception as e:
+            mb.showerror("Ошибка",f"Произошла ошибка: {e}")
+    else:
+        mb.showwarning("Внимание"," Введите код валюты")
 
 
 window = Tk()
